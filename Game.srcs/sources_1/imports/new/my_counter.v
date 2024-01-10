e6017f199c54d65622aacaf8d0658baee49c61a6
komaljoshi2203 <109128870+komaljoshi2203@users.noreply.github.com>
2024-01-10 17:32:43 +0530

    Update my_counter.v

--- Game.srcs/sources_1/imports/new/my_counter.v
+++ Game.srcs/sources_1/imports/new/my_counter.v
@@ -1,22 +1,8 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/19/2020 11:25:13 AM
-// Design Name: 
 // Module Name: my_counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Name: Komal Joshi
+//Roll No.: 21PHC1R16
 //////////////////////////////////////////////////////////////////////////////////
 
 
