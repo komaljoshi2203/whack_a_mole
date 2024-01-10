c586bb39ae57354277d56e853d10a193c6d8e06b
komaljoshi2203 <109128870+komaljoshi2203@users.noreply.github.com>
2024-01-10 17:33:03 +0530

    Update timerCount.v

--- Game.srcs/sources_1/imports/new/timerCount.v
+++ Game.srcs/sources_1/imports/new/timerCount.v
@@ -1,22 +1,8 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/26/2021 03:44:51 PM
-// Design Name: 
 // Module Name: timerCount
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Name: Komal Joshi
+//Roll No.: 21PHC1R16
 //////////////////////////////////////////////////////////////////////////////////
 
 
